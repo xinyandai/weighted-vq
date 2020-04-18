Fix bug: divide by zero. Dev: batch density calculation.

main.py
@@ -1,17 +1,23 @@
-import tqdm
 import math
 import numpy as np
 
+from tqdm import tqdm
 from vecs_io import loader
 from sorter import BatchSorter
 from weighted_vq import weighted_kmeans
 
 
-def all_density(x):
+def all_density(x, bs=2**12, verbose=True):
+    n, d = x.shape
+    kernel = np.empty(shape=n, dtype=x.dtype)
     x = np.sum(x**2, axis=1, keepdims=True)
-    d = -2.0 * (x @ x.T) + x + x.T
-    kernel = np.exp(-d / 2.0)
-    return np.sum(kernel, axis=1)
+    iter = n // bs + 1
+
+    for i in tqdm(range(iter)) if verbose else range(iter):
+        batch = x[i * bs : (i + 1) * bs]
+        d = -2.0 * np.dot(batch, x.T) + batch + x.T
+        kernel[i * bs : (i + 1) * bs] =np.sum(np.exp(-d / 2.0), axis=1)
+    return kernel
 
 
 def execute(compressed, X, Q, G, metric):
@@ -33,7 +39,8 @@ if __name__ == '__main__':
     Ks = 256
     metric = 'product'
 
-    X, T, Q, G = loader(dataset, topk, metric, folder='../../data/')
+    X, _, Q, G = loader(dataset, topk, metric, folder='../../data/')
+    assert G is not None
 
     density = all_density(X)
     centroid, codes = weighted_kmeans(X, w=density, k=256, iter=20, minit='points')

weighted_vq.py
@@ -56,16 +56,19 @@ def weighted_kmeans(data, w, k, iter=10, minit='random',
         else:
             code_book = init_meth(data, k)
 
+    cluster_means = _vq.update_cluster_means
     for _ in tqdm(range(iter)) if verbose else range(iter):
         # Compute the nearest neighbor for each obs using the current code book
         label = _vq.vq(data, code_book)[0]
         # Update the code book by computing centroids
         if w is None:
-            new_code_book, has_members = _vq.update_cluster_means(data, label, nc)
+            new_code_book, has_members = cluster_means(data, label, nc)
         else:
-            new_code_book, has_members = _vq.update_cluster_means(data * w, label, nc)
-            density_sum, _ = _vq.update_cluster_means(w, label, nc)
-            new_code_book /= density_sum
+            new_code_book, has_members = cluster_means(data * w, label, nc)
+            density_sum, _ = cluster_means(w, label, nc)
+            new_code_book = np.divide(new_code_book,
+                                      density_sum,
+                                      where=density_sum != 0)
         if not has_members.all():
             miss_meth()
             # Set the empty clusters to their previous positions